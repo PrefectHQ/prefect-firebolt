b2948bca205a52bcc20a9874d0c22ab08543b66e
Andrew <15331990+ahuang11@users.noreply.github.com>
2023-02-07 11:44:01 -0800

    Add analytics (#19)
    * Add analytics
    
    * Update .pre-commit-config.yaml
    
    * Update credentials.py
    
    * Update database.py
    
    * Update publish-docs.yml

--- prefect_firebolt/credentials.py
+++ prefect_firebolt/credentials.py
@@ -22,6 +22,7 @@ class FireboltCredentials(Block):
 
     _block_type_name = "Firebolt Credentials"
     _logo_url = "https://images.ctfassets.net/gm98wzqotmnx/3loU17IXqVIWl4aWQfqc78/3c7eefe5e8cf4eec870856f10d7fdcce/5e8a264ceaf4870c90478037_Favicon_128.svg.png?h=250"  # noqa
+    _documentation_url = "https://prefecthq.github.io/prefect-firebolt/credentials/#prefect_firebolt.credentials.FireboltCredentials"  # noqa
 
     @root_validator
     def _ensure_valid_auth_method(cls, values):
